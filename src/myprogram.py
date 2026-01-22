769ed119529ee36c171d65edf41cde84a6c72287
Jack Li <jackpli@attu4.cs.washington.edu>
2026-01-21 16:54:46 -0800

    First draft shakespeare, trl

--- src/myprogram.py
+++ src/myprogram.py
@@ -3,18 +3,44 @@ import os
 import string
 import random
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from trl import GRPOTrainer
+from datasets import load_dataset
+from trl.rewards import accuracy_reward
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+import wandb
+import random
+import logging
+import re
+import torch
 
+# Initialize logging
+logging.basicConfig(
+    level=logging.INFO,  # Set the logging level to INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",  # Log format
+    handlers=[
+        logging.StreamHandler(),  # Output to console
+        logging.FileHandler("nlp_run1.log")  # Output to a file
+    ]
+)
+LOGGER = logging.getLogger(__name__)
+
+# Initialize Weights & Biases
+wandb.init(project="CSE447")
+
+# MODEL NAME 
+model_name = "Qwen/Qwen3-0.6B"
 
 class MyModel:
     """
     This is a starter model to get you started. Feel free to modify this file.
     """
-
+    def __init__(self):
+        self.trainer = None
     @classmethod
     def load_training_data(cls):
-        # your code here
-        # this particular model doesn't train
-        return []
+        # Shakespeare next character prediction dataset
+        train_dataset = load_dataset("flwrlabs/shakespeare", split="train").shuffle(seed=42)
+        return train_dataset['x'], train_dataset['y']
 
     @classmethod
     def load_test_data(cls, fname):
@@ -33,17 +59,28 @@ class MyModel:
                 f.write('{}\n'.format(p))
 
     def run_train(self, data, work_dir):
-        # your code here
-        pass
+        train_data = self.load_training_data()
+        self.trainer = GRPOTrainer(
+            model=model_name,
+            reward_funcs=accuracy_reward,
+            train_dataset=train_data,
+        )
+        self.trainer.train()
 
     def run_pred(self, data):
-        # your code here
+        # inference
         preds = []
-        all_chars = string.ascii_letters
+        self.trainer.model.eval()
         for inp in data:
-            # this model just predicts a random character each time
-            top_guesses = [random.choice(all_chars) for _ in range(3)]
-            preds.append(''.join(top_guesses))
+            message = [
+                {"role": "system", "content": "Please reason step by step, and put your final answer within \\boxed{}."},
+                {"role": "user", "content": "Generate the next three most likely unicode characters to follow the input. The input is not necessarily in English, it may be in any human language."+inp}
+            ]
+            with torch.no_grad():
+                response = self.trainer.model.generate(message, max_new_tokens=16384, temperature=0.6, top_p=0.7, top_k=50,)
+                # identify text within the boxed region
+                pred_text = re.search(r'\\boxed\{(.*?)\}', response[0]['generation']['content'], re.DOTALL).group(1)
+                preds.append(pred_text)
         return preds
 
     def save(self, work_dir):
